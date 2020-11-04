7b5051524315aa01e64aba27120b2220cc122b69
Steven Lu <tacticalazn@gmail.com>
2020-11-04 15:50:25 -0500

    wtf time is this

---
+++ index.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    input: '',
+    output: ''
+  },
+  methods: {
+    convert: function () {
+      var m = moment()
+
+      if (_.parseInt(this.input)) {
+        if (this.input.length <= 10) {
+          m = moment(this.input, 'X')
+        }
+      } else {
+        m = moment(this.input)
+      }
+
+      if (!this.input) {
+        this.output = ''
+      } else if (!m.isValid()) {
+        this.output = '???'
+      } else {
+        this.output = m.format('dddd, MMMM D, YYYY h:mm A')
+      }
+    }
+  }
+})
